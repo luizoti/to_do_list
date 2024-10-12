39ba0388ad44465a854d345f3073017fdbd50033
Luiz Antonio Lazoti <luiz.lazoti@youtan.com.br>
2024-10-11 23:11:55 -0300

    feat: include a ModelManager to Tasks

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 """The main FastApi module."""
-import logging
 
-import uvicorn
-from fastapi import FastAPI
-
-from to_do_list.crud import ROUTER
 from fastapi import FastAPI, Request
-
-from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from fastapi import APIRouter, Depends
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
+from to_do_list.api.v1 import ROUTER
+
 # from to_to_list.database import ImageHistoryManager, session_clear
 
 ports = [
@@ -29,7 +22,7 @@ def generate_origins():
     """Function with objective to create a list of allowed origins."""
     for host in hosts:
         host_name = host.get("host")
-        https = "https://" if host.get("https") else "http://" # noqa
+        https = "https://" if host.get("https") else "http://"  # noqa
         if host.get("ports"):
             for port in ports:
                 yield f"{https}{host_name}:{port}"
@@ -42,7 +35,7 @@ FAST_API_APP = FastAPI()
 # FAST_API_APP = FastAPI(dependencies=[Depends(session_clear)])
 
 FAST_API_APP.add_middleware(
-    CORSMiddleware, # noqa
+    CORSMiddleware,  # noqa
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
@@ -52,11 +45,13 @@ FAST_API_APP.add_middleware(
 FAST_API_APP.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+
 @FAST_API_APP.get("/")
 async def index(request: Request):
     """Application index page."""
     return templates.TemplateResponse(
         request=request, name="index.html"
     )
+
 
 FAST_API_APP.include_router(ROUTER)
--- to_do_list/database.py
+++ to_do_list/database.py
@@ -10,6 +10,7 @@ from sqlalchemy import (
     String,
     Boolean
 )
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 from to_do_list.settings import DATABASE_URL
@@ -20,11 +21,13 @@ engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+
 def session_clear(exception=None):
     """Clear season if active."""
     session.close_all()
     if exception and session.is_active:
         session.rollback()
+
 
 session_clear()
 
@@ -32,8 +35,9 @@ session_clear()
 class TaskStates(Enum):
     """Task states."""
     PENDENTE = 0
-    CONCLUIDA = 1 # noqa
+    CONCLUIDA = 1  # noqa
     EM_PROGRESSO = 2
+
 
 class TaskPrioritys(Enum):
     """Task prioritys."""
@@ -41,16 +45,101 @@ class TaskPrioritys(Enum):
     MEDIA = 1
     ALTA = 2
 
+
 class Tasks(Base):
     __tablename__ = "Tasks"
-    id = Column(Integer, primary_key=True, autoincrement=True, name="ID da task")
-    created_at = Column(DateTime, nullable=False, name="Data de Criação")
-    title = Column(String, nullable=False, name="Titulo")
-    description = Column(String, nullable=False, name="Descrição")
-    priority = Column(Integer, nullable=False, name="Prioridade")
-    completion_date = Column(DateTime, nullable=False, name="Data de conclusão")
-    category = Column(String, nullable=False, name="Categoria")
-    order = Column(Integer, nullable=False, name="Ordem")
-    concluded = Column(Boolean, nullable=False, name="Concluída")
+    id = Column(Integer, primary_key=True, name="id", comment="ID da task")
+    created_at = Column(DateTime, nullable=False, name="created_at", comment="Data de Criação")
+    title = Column(String, nullable=False, name="title", comment="Titulo")
+    description = Column(String, nullable=False, name="description", comment="Descrição")
+    priority = Column(Integer, nullable=False, name="priority", comment="Prioridade")
+    completion_date = Column(DateTime, nullable=False, name="completion_date", comment="Data de conclusão")
+    category = Column(String, nullable=False, name="category", comment="Categoria")
+    order = Column(Integer, nullable=False, name="order", comment="Ordem")
+    concluded = Column(Boolean, nullable=False, name="concluded", comment="Concluída")
+
 
 Base.metadata.create_all(engine)
+
+
+class TasksManager:
+    """Manager for handling database operations on a model."""
+
+    model = Tasks  # Atributo de classe a ser definido nos modelos concretos
+
+    @classmethod
+    def _to_dict(cls, obj):
+        """Convert SQLAlchemy object to dictionary."""
+        return {column.name: getattr(obj, column.name) for column in obj.__table__.columns}
+
+    @classmethod
+    def create(cls, **kwargs):
+        """Create a new instance of the model and return it as a dict."""
+        try:
+            obj = cls.model(**kwargs)
+            session.add(obj)
+            session.commit()
+            return cls._to_dict(obj)
+        except SQLAlchemyError as e:
+            session.rollback()
+            return {"success": False, "message": str(e)}
+
+    @classmethod
+    def update(cls, obj_id, **kwargs):
+        """Update an existing instance of the model and return it as a dict."""
+        try:
+            obj = session.query(cls.model).filter_by(id=obj_id).first()
+            if not obj:
+                return {"success": False, "message": "Object not found."}
+
+            for key, value in kwargs.items():
+                setattr(obj, key, value)
+
+            session.commit()
+            return cls._to_dict(obj)
+        except SQLAlchemyError as e:
+            session.rollback()
+            return {"success": False, "message": str(e)}
+
+    @classmethod
+    def delete(cls, obj_id):
+        """Delete an existing instance of the model."""
+        try:
+            obj = session.query(cls.model).filter_by(id=obj_id).first()
+            if not obj:
+                return {"success": False, "message": "Object not found."}
+            session.delete(obj)
+            session.commit()
+            return {"success": True, "message": "Object deleted successfully."}
+        except SQLAlchemyError as e:
+            session.rollback()
+            return {"success": False, "message": str(e)}
+
+    @classmethod
+    def get(cls, obj_id):
+        """Retrieve a single instance of the model by ID and return it as a dict."""
+        try:
+            obj = session.query(cls.model).filter_by(id=obj_id).first()
+            if obj:
+                return cls._to_dict(obj)
+            return {}
+        except SQLAlchemyError as e:
+            return {"success": False, "message": str(e)}
+
+    @classmethod
+    def all(cls):
+        """Retrieve all instances of the model and return them as a list of dicts."""
+        try:
+            objects = session.query(cls.model).all()
+            return [cls._to_dict(obj) for obj in objects]
+        except SQLAlchemyError as e:
+            return {"success": False, "message": str(e)}
+
+    @classmethod
+    def filter(cls, **filters):
+        """Filter instances of the model based on given filters and return them as a list of dicts."""
+        try:
+            objects = session.query(cls.model).filter_by(**filters).all()
+            return [cls._to_dict(obj) for obj in objects]
+        except SQLAlchemyError as e:
+            return {"success": False, "message": str(e)}
